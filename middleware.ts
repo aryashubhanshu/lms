49697fae87a3227f36218b565c842b54a7b2db53
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-07-02 22:43:55 +0530

    Auth using Clerk Setup

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Define routes that should be protected
+const isProtectedRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
