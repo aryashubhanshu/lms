51fd94096e172700a66385f7856e2e81d311b30f
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-07-03 22:20:16 +0530

    Course Createion UI

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Coures</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default page;
