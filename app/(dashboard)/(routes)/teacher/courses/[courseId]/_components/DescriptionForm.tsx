39ee3a898f21f0c8daec9908a90850c4cb10d379
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-07-04 23:26:30 +0530

    Uploadthing and Image Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/DescriptionForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/DescriptionForm.tsx
@@ -18,11 +18,10 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
+import { Course } from "@prisma/client";
 
 interface DescriptionFormProps {
-  initialData: {
-    description: string;
-  };
+  initialData: Course;
   courseId: string;
 }
 
@@ -42,7 +41,9 @@ const DescriptionForm = ({ initialData, courseId }: DescriptionFormProps) => {
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData,
+    defaultValues: {
+      description: initialData?.description || "",
+    },
   });
 
   const { isSubmitting, isValid } = form.formState;
